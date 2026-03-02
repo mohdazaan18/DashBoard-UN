55a2c446c5e3198b4284e066108710f982ee4fa0
mohdazaan <mohdazaan293@gmail.com>
2026-03-02 09:46:52 +0530

    removed helper files

--- test_groups.ts
+++
@@ -1,4 +0,0 @@
-import { GROUPS } from "./backend/src/constants/groups";
-console.log(Object.values(GROUPS).flat().length);
-const allCountries = new Set(Object.values(GROUPS).flat());
-console.log("Has India?", allCountries.has("India"));
